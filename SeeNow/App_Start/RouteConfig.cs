3956e9cdda3af39067bf9290b86998070419c706
Meta Jheng <sailoruranu@hotmail.com>
2019-05-09 11:46:00 +0800

    20190509001 整合Legai的前端首頁與註冊 調整後端設定

--- SeeNow/App_Start/RouteConfig.cs
+++ SeeNow/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SeeNow
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "backendHome", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ SeeNow/Controllers/UsersController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SeeNow.Models;
+
+namespace SeeNow.Controllers
+{
+    public class UsersController : Controller
+    {
+        private SeeNowEntities db = new SeeNowEntities();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var users = db.users.Include(u => u.profile).Include(u => u.role);
+            return View(users.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            users users = db.users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(users);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            ViewBag.profile_id = new SelectList(db.profile, "profile_id", "profile_name");
+            ViewBag.role_id = new SelectList(db.role, "role_id", "role_desc");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "account,role_id,password,nick_name,e_mail,score,energy,profile_id,bag_number,lock_flag,validation_flag,resetable")] users users)
+        {
+            if (ModelState.IsValid)
+            {
+                db.users.Add(users);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.profile_id = new SelectList(db.profile, "profile_id", "profile_name", users.profile_id);
+            ViewBag.role_id = new SelectList(db.role, "role_id", "role_desc", users.role_id);
+            return View(users);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            users users = db.users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.profile_id = new SelectList(db.profile, "profile_id", "profile_name", users.profile_id);
+            ViewBag.role_id = new SelectList(db.role, "role_id", "role_desc", users.role_id);
+            return View(users);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "account,role_id,password,nick_name,e_mail,score,energy,profile_id,bag_number,lock_flag,validation_flag,resetable")] users users)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(users).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.profile_id = new SelectList(db.profile, "profile_id", "profile_name", users.profile_id);
+            ViewBag.role_id = new SelectList(db.role, "role_id", "role_desc", users.role_id);
+            return View(users);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            users users = db.users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(users);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            users users = db.users.Find(id);
+            db.users.Remove(users);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
