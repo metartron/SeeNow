fba063c1e0e2cb468fa1f51e325c94cafd5056c5
Meta Jheng <sailoruranu@hotmail.com>
2019-05-17 11:18:42 +0800

    20190517002
    更新忽略的Extention class (Meta 測試中)

---
+++ SeeNow/Extention/Extention.String.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace SeeNow
+{
+	public static partial class Extention
+    {
+		/// <summary>
+        /// 取出相對key值的value
+        /// </summary>
+        /// <param name="PropertyName">key值</param>
+        /// <param name="o">傳入物件</param>
+        /// <returns>value值</returns>
+        public static Object GetPropertyValueByName(string PropertyName, Object o)
+        {
+            if (o == null)
+            {
+                o = new { };
+            }
+            
+            Object returnObject = new Object();
+            PropertyInfo[] p1 = o.GetType().GetProperties();
+            foreach (PropertyInfo pi in p1)
+            {
+                if (pi.Name.ToLower() == PropertyName.ToLower())
+                {
+                    returnObject = pi.GetValue(o);
+                }
+            }
+            return returnObject;
+        }
+    }
+}
---
+++ SeeNow/Extention/Validation.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace SeeNow
+{
+    public class Validation
+    {
+        #region RandomCode 隨機生成指定長度的驗證碼字符串
+        private string RandomCode(int length)
+        {
+            
+            string s = "23456789zxcvbnmasdfghjkqwertyuip";
+            StringBuilder sb = new StringBuilder();
+            Random rand = new Random();
+            int index;
+            for (int i = 0; i < length; i++)
+            {
+                index = rand.Next(0, s.Length);
+                sb.Append(s[index]);
+            }
+            return sb.ToString();
+        }
+        #endregion
+
+        #region PaintInterLine 產生刪除線 num 代表幾條
+        private void PaintInterLine(Graphics g, int num, int width, int height)
+        {
+            Random r = new Random();
+            int startX, startY, endX, endY;
+            for (int i = 0; i < num; i++)
+            {
+                startX = r.Next(0, width);
+                startY = r.Next(0, height);
+                endX = r.Next(0, width);
+                endY = r.Next(0, height);
+                g.DrawLine(new Pen(Brushes.Red), startX, startY, endX, endY);
+            }
+        }
+        #endregion
+
+        #region GetValidateCode 畫出 圖形驗證碼
+        public void GetValidateCode(out string code,out byte[] data)
+        {
+            //byte[] data = null;
+            //string code = RandomCode(5);
+            data = null;
+            code = RandomCode(5);
+            //TempData["code"] = code;
+            //定義一個畫板
+            MemoryStream ms = new MemoryStream();
+            using (Bitmap map = new Bitmap(100, 40))
+            {
+                //畫筆,在指定畫板畫板上畫圖
+                //g.Dispose();
+                using (Graphics g = Graphics.FromImage(map))
+                {
+                    g.Clear(Color.White);
+                    g.DrawString(code, new Font("黑體", 18.0F), Brushes.Blue, new Point(10, 8));
+                    //繪製干擾線(數字代表幾條)
+                    PaintInterLine(g, 10, map.Width, map.Height);
+                }
+                map.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            data = ms.GetBuffer();
+            //File(data, "image/jpeg");
+        }
+        #endregion
+    }
+}
