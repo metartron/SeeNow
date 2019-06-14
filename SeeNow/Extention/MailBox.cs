31a202ddc1787cb5ac52cffa965ee28177bbe6a0
Meta <metartron@gmail.com>
2019-06-14 11:07:22 +0800

    20190614001 Meta

---
+++ SeeNow/Extention/MailBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace SeeNow
+{
+    public static partial class MailBox
+    {
+        public static void SendMail( string account,string toMail,string subject,string body )
+        {
+            SmtpClient myMail = new SmtpClient("msa.hinet.net");
+            MailAddress from = new MailAddress("metartron+seenow@gmail.com", "SeeNow服務中心");
+            MailAddress to = new MailAddress(toMail);
+            MailMessage Msg = new MailMessage(from, to);
+            Msg.Subject = subject;
+            Msg.Body = body;
+
+            Msg.IsBodyHtml = true;
+
+            myMail.Send(Msg);
+        }
+
+    }
+}
