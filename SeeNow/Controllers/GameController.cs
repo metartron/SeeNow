aa5debb63422e2ec74c8ccdf71dcf4744b317399
Meta <metartron@gmail.com>
2019-06-26 14:42:15 +0800

    20190626002 Robin遊戲時 答案若不是四個就不顯示

--- SeeNow/Controllers/GameController.cs
+++ SeeNow/Controllers/GameController.cs
@@ -27,43 +27,49 @@ namespace SeeNow.Controllers
                 quiz = quiz.Where(s => s.quiz_group.ToString().Contains(id));
             }
 
+            //join quiz_answer 和 quiz 把不是4組答案的題目過瀘,讓題目不出現
+            var quiz_quid = (from qzas in db.quiz_answer
+                             join qz in quiz on qzas.quiz_guid equals qz.quiz_guid
+                             group qz by new { qz.quiz_guid, qz.tittle_text } into g
+                             where g.Count() == 4
+                             select new { g.Key.quiz_guid, g.Key.tittle_text }).ToList();
+
             //JOIN 所選題目+4組答案
-            var answer_text = (from qz in quiz
-                               join qzas in db.quiz_answer on qz.quiz_guid equals qzas.quiz_guid
-                               //select new Quiz4Answers { quizzes = qz, quiz_answer = qzas };
-                               select new { quizzes = qz, quiz_answer = qzas }).ToList();
             //answer_text內容為title4組+ans4組
             //{"title1":"10+10","ans1":"24","title2":"100+100","ans2":"201","title3":"100+100","ans3":"203"}
             //利用Dictionary<string, object>產出(key,value)
-            //strObj.Add會有重覆title key出現,產生錯誤時用catch默許錯誤,只做strObj.Add("ans"..
             //產出的List將只有留{"title":"10+10","ans1":"24","ans2":"201","ans3":"203"}
             int i = 1;
             Dictionary<string, object> strObj = new Dictionary<string, object>();
             List<Dictionary<string, object>> qzansList = new List<Dictionary<string, object>>();
-            foreach (var qz in answer_text)
+            foreach (var q_id in quiz_quid)
             {
+                var qz = db.quizzes.Where(q => q.quiz_guid == q_id.quiz_guid)
+                           .SingleOrDefault();
                 try
                 {
-                    strObj.Add("quiz_guid", qz.quizzes.quiz_guid);
-                    strObj.Add("title", qz.quizzes.tittle_text);
-                    strObj.Add("img", qz.quizzes.title_img_path);
-                    strObj.Add("time", qz.quizzes.time);
-                    strObj.Add("score", qz.quizzes.score);
+                    strObj.Add("quiz_guid", qz.quiz_guid);
+                    strObj.Add("title", qz.tittle_text);
+                    strObj.Add("img", qz.title_img_path);
+                    strObj.Add("time", qz.time);
+                    strObj.Add("score", qz.score);
                 }
                 catch (Exception ex)
                 {
                     ex.ToString();
                 }
 
-                strObj.Add("ans" + i, qz.quiz_answer.answer_text);
-                strObj.Add("is_correct" + i, qz.quiz_answer.is_correct);
-                i++;
-                if (i > 4)
+                var qzans = db.quiz_answer.Where(q => q.quiz_guid == q_id.quiz_guid)
+                   .ToList();
+
+                foreach (var qzan in qzans)
                 {
-                    qzansList.Add(strObj);
-                    strObj = new Dictionary<string, object>();
-                    i = 1;
+                    strObj.Add("ans" + i, qzan.answer_text);
+                    strObj.Add("is_correct" + i, qzan.is_correct);
+                    i++;
                 }
+                qzansList.Add(strObj);
+                strObj = new Dictionary<string, object>();
             }
 
             //取出目前round_no最大的一筆,用ViewBag.round_no傳給View用,
@@ -161,65 +167,7 @@ namespace SeeNow.Controllers
             return View("ScoreView", allScoreList);
         }
 
-
-
-        //測試用,直接開TestView撈資料庫特定id(round_no)
-        //public ActionResult TestView()
-        //{
-        //    var id = "211:215:217:216:218:222:224:225:226:228:229";
-        //    string[] rounds = id.Split(':');
-        //    string strRound = "";
-
-        //    List<play_record> allScoreList = new List<play_record>();
-        //    //這一局的所有題組參加的players放入scoreList
-        //    //最前的一組是pin r=1,最後一組空白不要r<=rounds.Length-2
-        //    for (var r = 0; r <= rounds.Length - 1; r++)
-        //    {
-        //        var r_no = int.Parse(rounds[r]);
-        //        var scoreList = (from score in db.play_record
-        //                         where score.round_no == r_no
-        //                         select score).ToList();
-        //        strRound += r_no + ";";
-        //        //將scoreList的list累加到allScoreList
-        //        foreach (var all in scoreList)
-        //        {
-        //            allScoreList.Add(all);
-        //        }
-        //    }
-        //    //scoreList中相同account的score相加,並Descending
-        //    var userSort = allScoreList
-        //    .GroupBy(t => t.account)
-        //    .Select(grp => new { grp.First().account, score = grp.Sum(t => t.score) })
-        //    .OrderByDescending(a => a.score);
-
-        //    //usr.account, usr.role_id, usr.password, usr.nick_name, usr.e_mail, usr.score, usr.energy, usr.profile_id, }))
-        //    users usr = db.users.Find("test2019");
-        //    //usr.score = u.score;
-        //    usr.score = 12345;
-        //    try
-        //    {
-        //        //db.SaveChanges();
-        //    }
-        //    catch (Exception dex)
-        //    {
-        //        //Log the error (uncomment dex variable name and add a line here to write a log.
-        //        ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
-        //    }
-
-
-
-        //    var user_profile_score = (from us in userSort
-        //                              join u in db.users on us.account equals u.account
-        //                              join p in db.profile on u.profile_id equals p.profile_id
-        //                              select new { pf = p.profile_path, acc = u.account, sco = us.score })
-        //                             .ToList();
-
-        //    //以ViewBag.Round傳前頁面來的Pin碼,當ScoreView頁面開啟時,執行seeScore.js把round填入,並啟動新的signalR
-        //    ViewBag.Round = rounds[0];
-        //    ViewBag.strRound = strRound;
-        //    ViewBag.userSort = user_profile_score;
-        //    return View("TestView", allScoreList);
-        //}
+        
         public ActionResult TestView()
         {
             
@@ -230,5 +178,67 @@ namespace SeeNow.Controllers
             ViewBag.u_list = user;
             return View();
         }
+        public ActionResult QuizView(string id)
+        {
+            var quiz = from m in db.quizzes
+                       select m;
+            if (!String.IsNullOrEmpty(id))
+            {
+                //從quizzes取出包含id的題目
+                quiz = quiz.Where(s => s.quiz_group.ToString().Contains(id));
+            }
+
+
+            var quiz_quid = (from qzas in db.quiz_answer
+                             join qz in quiz on qzas.quiz_guid equals qz.quiz_guid
+                             group qz by new { qz.quiz_guid, qz.tittle_text } into g
+                             where g.Count() == 4
+                             select new { g.Key.quiz_guid, g.Key.tittle_text }).ToList();
+            int i = 1;
+            Dictionary<string, object> strObj = new Dictionary<string, object>();
+            List<Dictionary<string, object>> qzansList = new List<Dictionary<string, object>>();
+            foreach (var q_id in quiz_quid)
+            {
+                var qz = db.quizzes.Where(q => q.quiz_guid == q_id.quiz_guid)
+                           .SingleOrDefault();
+                try
+                {
+                    strObj.Add("quiz_guid", qz.quiz_guid);
+                    strObj.Add("title", qz.tittle_text);
+                    strObj.Add("img", qz.title_img_path);
+                    strObj.Add("time", qz.time);
+                    strObj.Add("score", qz.score);
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                }
+
+                var qzans = db.quiz_answer.Where(q => q.quiz_guid == q_id.quiz_guid)
+                   .ToList();
+
+                foreach (var qzan in qzans)
+                {
+                    strObj.Add("ans" + i, qzan.answer_text);
+                    strObj.Add("is_correct" + i, qzan.is_correct);
+                    i++;
+                }
+                qzansList.Add(strObj);
+                strObj = new Dictionary<string, object>();
+            }
+
+            //JOIN 所選題目+4組答案
+
+            //var answer_text = db.quiz_answer.Join(db.quizzes, qa => qa.quiz_guid, qz => qz.quiz_guid,
+            //    (qa, qz) => new {qzid = qz.quiz_guid, title =qz.tittle_text,answer = qa.answer_text,qaid=qa.quiz_guid  });
+
+            string jsonString = JsonConvert.SerializeObject(qzansList);
+            ViewBag.qzans = jsonString;
+            
+
+            return View();
+            
+        }
+    
     }
 }
